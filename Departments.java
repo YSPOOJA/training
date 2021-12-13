56a948d0da345db459893ed9e1b4d7101f5a8821
YSPOOJA <95734736+YSPOOJA@users.noreply.github.com>
2021-12-13 10:34:39 +0530

    Delete Departments.java

--- Departments.java
+++
@@ -1,9 +0,0 @@
-class Departments {
-   int DEPARTMENT_ID;
-   String DEPARTMENT_NAME;
-   int MANAGER_ID;
-   int LOCATION_ID;
-   public static void main(String[] args) {
-    System.out.println("table");
-}
-}
