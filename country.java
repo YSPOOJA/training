89c5e9ca77c12d917c29739eea968a428df0ae63
YSPOOJA <95734736+YSPOOJA@users.noreply.github.com>
2021-12-13 10:35:45 +0530

    Delete country.java

--- country.java
+++
@@ -1,9 +0,0 @@
-class country {
-    
-    int COUNTRY_ID;
-    String COUNTRY_NAME;
-    int REGION_ID; 
-    public static void main(String[] args) {
-        System.out.println("table");
-    }
-}
